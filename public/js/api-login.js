dab389f1ac6e37fc4df2ff0cc65192dd7310b492
Imran Khan <Imran.khan@synergy.it>
2018-10-18 12:15:12 +0500

    First commit

---
+++ public/js/api-login.js
@@ -0,0 +1,98 @@
+let settings = {
+   clientId : 'ynicj7x4xq',
+   clientSecret : 'xqj4zj0qf3aruwm7z9j2kellbyg40m',
+   oAuthUrl : 'https://oauth.wildapricot.org/auth/token'
+
+
+}
+
+var userData = {};
+
+var loginData = {
+    grant_type : 'password',
+    username : 'imranqau5373@gmail.com',
+    password : 'imrankhan',
+    scope : 'auto'
+};
+
+const otherParms = {
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        "Authorization": "Basic " + window.btoa(settings.clientId +':'+settings.clientSecret)
+      },
+      body : loginData,
+      method:"POST"
+      
+}
+
+
+
+
+
+
+
+function loginOnApi()
+{
+    
+    // loginData.username = $('#inputUsername').val();
+    // loginData.password = $('#inputPassword').val();
+
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://oauth.wildapricot.org/auth/token",
+        "method": "POST",
+        "headers": {
+          "authorization": "Basic eW5pY2o3eDR4cTp4cWo0emowcWYzYXJ1d203ejlqMmtlbGxieWc0MG0=",
+          "content-type": "application/x-www-form-urlencoded",
+          "cache-control": "no-cache",
+          "postman-token": "74909c41-09cf-a4c4-4a4c-e377f9586951"
+        },
+        "data": ""
+      }
+      debugger;
+
+
+      var data = 'grant_type=password&username=imranqau5373@gmail.com&password=imrankhan&scope=auto';
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "https://oauth.wildapricot.org/auth/token");
+xhr.setRequestHeader("authorization", "Basic eW5pY2o3eDR4cTp4cWo0emowcWYzYXJ1d203ejlqMmtlbGxieWc0MG0=");
+xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.setRequestHeader("postman-token", "8a1b1f11-ab8c-3a96-cb55-34bc20ef4afe");
+
+xhr.send(data);
+      
+    //   $.ajax({
+    //     type: 'POST',
+    //     url: 'https://oauth.wildapricot.org/auth/token',
+    //     data: JSON.stringify(loginData),
+    //     dataType: "json",
+    //     headers: {
+    //        'Authorization': "Basic " + window.btoa(settings.clientId +':'+settings.clientSecret),
+    //        "content-type": "application/x-www-form-urlencoded",
+    //        "cache-control": "no-cache",
+    //     },
+    //     success: function (result) {
+    //        var token = result;
+    //     },
+    //     //complete: function (jqXHR, textStatus) {
+    //     //},
+    //     error: function (req, status, error) {
+    //         debugger;
+    //     alert(error);
+    //     }
+    // });
+
+
+}
---
+++ routes/login.js
@@ -0,0 +1,66 @@
+
+
+const request = require('request');
+var express = require('express');
+var router = express.Router();
+var session = require('express-session');
+
+let settings = {
+  clientId : 'xcqzmrmdhs',
+  clientSecret : 'vhy0lod2ssttqpofycxshus0v7roes',
+  oAuthUrl : 'https://oauth.wildapricot.org/auth/token'
+
+
+}
+var responseData = {};
+
+var loginData = {
+  username : '',
+  password : ''
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('api-login', { title: 'User Login' });
+});
+
+router.get('/logout', function(req, res, next) {
+  // res.cookie('auth','');
+  res.redirect('/login');
+});
+
+router.post('/', function(req, res, next) {
+  loginData.username = req.body.username;
+  loginData.password = req.body.password;
+  
+  
+  request.post(settings.oAuthUrl, {
+    form: {
+      grant_type: 'password',
+      username : loginData.username,
+      password : loginData.password,
+      scope: 'auto'
+    },
+    headers: {
+       'content-type': 'application/x-www-form-urlencoded',
+       'Accept': 'application/json',
+       "Authorization": "Basic " + new Buffer(settings.clientId+":"+settings.clientSecret).toString('base64')
+     },
+    json: true
+  }, function (err, response, body) {
+    if( err) {
+      console.error(err);
+      res.status(400);
+      res.json({error : "Bad Request"});
+    }
+    else {
+      res.status(200);
+      res.json(body);
+    }
+  })
+
+  });
+
+
+
+module.exports = router;
